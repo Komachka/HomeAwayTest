30e55630b0a9d4e1ae02a3b5daecc5ebe0433dba
Kateryna Storozh <kstor@softserveinc.com>
2019-09-13 15:49:57 +0300

    create retrofit service to get venueses data from api

---
+++ app/src/main/java/com/kstor/homeawaytest/data/Const.kt
@@ -0,0 +1,13 @@
+package com.kstor.homeawaytest.data
+
+const val CLIENT_ID = "FVKVXRRGCLEW0VUADJQVIEHKMWIGURSZV1QM4GUHWVJFT1QT"
+const val CLIENT_SECRET = "LHTQZG3SSVF1PBTIMZUCWV3JX5ED3ZPCICM0LWB4HVFZUUEU"
+
+const val BASE_URL = "https://api.foursquare.com/v2/venues/"
+const val SEARCH_URL = "search?"
+const val CLIENT_ID_QUERY_PARAM = "client_id"
+const val CLIENT_SECRET_QUERY_PARAM = "client_secret"
+const val NEAR_QUERY_PARAM = "near"
+const val SEARCH_QUERY_PARAM = "query"
+const val V_QUERY_PARAM = "v"
+const val LIMIT_QUERY_PARAM = "limit"
