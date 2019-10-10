7f96d7697c40b8d82ea8e381f22806acd68defc5
Kateryna Storozh <storozhkateryna@gmail.com>
2019-10-10 16:51:37 +0300

    update remote data

--- app/src/main/java/com/kstor/homeawaytest/data/network/RemoteData.kt
+++ app/src/main/java/com/kstor/homeawaytest/data/network/RemoteData.kt
@@ -3,13 +3,14 @@ package com.kstor.homeawaytest.data.network
 import com.kstor.homeawaytest.data.*
 import com.kstor.homeawaytest.data.network.model.NetworkDetailsModel
 import com.kstor.homeawaytest.data.network.model.NetworkVenuesModel
+import retrofit2.Response
 import java.io.IOException
 import java.lang.Exception
 
 class RemoteData(private val venuesService: VenuesService) {
     suspend fun closedVenues(limit: Int, query: String): ApiResult<NetworkVenuesModel> {
-        try {
-            val responce = venuesService.getVenusesNetworkData(
+        return apiCall {
+            venuesService.getVenusesNetworkData(
                 CLIENT_ID,
                 CLIENT_SECRET,
                 NEAR,
@@ -17,20 +18,23 @@ class RemoteData(private val venuesService: VenuesService) {
                 V,
                 limit
             )
-            return if (responce.isSuccessful) ApiResult.Succsses(responce.body()!!)
-            else ApiResult.Error<NetworkVenuesModel>(IOException(responce.errorBody().toString()))
-        } catch (ex: Exception) {
-            return ApiResult.Error<NetworkVenuesModel>(ex)
         }
     }
 
     suspend fun getDetailsOfAVenue(id: String): ApiResult<NetworkDetailsModel> {
+        return apiCall {
+            venuesService.getDetailsOfAVenue(id, CLIENT_ID, CLIENT_SECRET, V)
+        }
+    }
+
+    private suspend fun <T : Any> apiCall(call: suspend () -> (Response<T>)): ApiResult<T> {
         try {
-          val responce = venuesService.getDetailsOfAVenue(id, CLIENT_ID, CLIENT_SECRET, V)
-          return if (responce.isSuccessful) ApiResult.Succsses(responce.body()!!)
-            else ApiResult.Error<NetworkDetailsModel>(IOException(responce.errorBody().toString()))
+            call.invoke().let { responce ->
+                return if (responce.isSuccessful) ApiResult.Succsses(responce.body()!!)
+                else ApiResult.Error<T>(IOException(responce.errorBody().toString()))
+            }
         } catch (ex: Exception) {
-            return ApiResult.Error<NetworkVenuesModel>(ex)
+            return ApiResult.Error<T>(ex)
         }
     }
 }
